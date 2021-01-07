f33aef51ba2f31745238190733eed01c112d6699
lmtbelmonte <lmtbelmonte@gmail.com>
2021-01-07 11:00:21 +0100

    inclusion de confirm_destroy y de auto_approve, Paara destroy ponemos 1 y true

--- main.tf
+++ main.tf
@@ -26,9 +26,11 @@ module "codepipeline" { #B
   source   = "./modules/codepipeline"
   name     = "terraform-in-action"
   vcs_repo = var.vcs_repo
+  auto_apply = true
   environment = {
     AWS_ACCESS_KEY_ID     = var.aws.access_key
     AWS_SECRET_ACCESS_KEY = var.aws.secret_key
+    CONFIRM_DESTROY = 1
   }
   s3_backend_config = module.s3backend.config
 }
